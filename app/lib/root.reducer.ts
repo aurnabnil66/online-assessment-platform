c7ca6c166e8d6b2068155fbcdef5e7af7d92038d
Aurnab Paul <105810364+aurnabnil66@users.noreply.github.com>
2026-04-10 23:08:13 +0600

    reducers added in root reducer

---
+++ app/lib/root.reducer.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import { authReducer } from "./slices/auth/auth.slice";
+import { examReducer } from "./slices/exam/exam.slice";
+import { candidateExamReducer } from "./slices/candidateExam/candidateExam.slice";
+
+export const rootReducer = combineReducers({
+  auth: authReducer,
+  exam: examReducer,
+  candidtaeExam: candidateExamReducer,
+});
