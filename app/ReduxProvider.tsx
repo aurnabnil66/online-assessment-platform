28f52dd143b4f1cad7d62f4c9873cc846d14ece3
Aurnab Paul <105810364+aurnabnil66@users.noreply.github.com>
2026-04-11 23:13:16 +0600

    modified redux provider

--- app/ReduxProvider.tsx
+++ app/ReduxProvider.tsx
@@ -3,12 +3,16 @@
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "@/lib/store";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <Header />
         {children}
+        <Footer />
       </PersistGate>
     </Provider>
   );
